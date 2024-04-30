52aab3ff8dfe55b78413a48ddbee07fc397ad4d4
arian <arian.ghb@gmail.com>
2024-04-30 19:02:27 +0330

    Refactored run.py, fixed no kill-signal detection, moved all sttings to .env

--- run.py
+++ run.py
@@ -8,31 +8,25 @@ from multiprocessing import Process
 from logging import Logger, config, getLogger
 from pathlib import Path
 from time import sleep
+from ast import literal_eval
 from src.scrapper import Scrapper
 from src.db import DB
 from src.utils import ScrapperException
 from src.contracts import JobData, Singleton
 
 
-# Dynamic Inputs
-# KEYWORDS = ["python data engineer","backend python software engineer","cloud engineer"]
-KEYWORDS = ["python data engineer","backend python software engineer","cloud engineer"]
-MAX_NUMBER_OF_JOBS = 100
-HEADLESS = True
-LOAD_TIMEOUT = 50
-USER_DATA_DIR = os.environ['CHROME_PROFILE']
-HIGH_CPU_THRESHOLD = 85
-MAX_HIGH_CPU_COUNT = 6
-MAX_SCRAPPER_RESTART_ATTEMPTS = 6
-
+# Config the logger. ** Must be done before all logging initializations
+logging_config_file_name = "src/logging_local.yml"
+with open(logging_config_file_name, 'r') as logging_config_file:
+    config.dictConfig(yaml.load(logging_config_file, Loader=yaml.FullLoader))
+# Load environment variables
+dotenv.load_dotenv(".env")
 
 class Runner(metaclass=Singleton):
     _logger: Logger
     _job_data: JobData
 
     def __init__(self):
-        # Load environment variables
-        dotenv.load_dotenv(".env")
         # Initialize logger config
         for folder in [
             os.environ["LOG_FOLDER"],
@@ -40,30 +34,26 @@ class Runner(metaclass=Singleton):
             os.environ["OUTPUT_FOLDER"]
             ]:
             Path(folder).mkdir(exist_ok=True)
-        logging_config_file_name = "src/logging_local.yml"
-        with open(logging_config_file_name, 'r') as logging_config_file:
-            config.dictConfig(yaml.load(logging_config_file, Loader=yaml.FullLoader))
         # Get the specific logger for scrapper #TODO: We later get specific logs for each component
         self._logger = getLogger("scrape")
         # Initialize database
         self._job_data = DB(db_name=os.environ["DB_NAME"],output_folder=os.environ['OUTPUT_FOLDER'])
     
-
     def run_scrapper(self):
         self._logger.info("---------------- Start a new crawl process ----------------")
         # Initialize scrapper
         scrapper = Scrapper(
             job_data=self._job_data,
-            max_n_jobs=MAX_NUMBER_OF_JOBS,
+            max_n_jobs=int(os.environ['MAX_NUMBER_OF_JOBS']),
             logger=self._logger,
-            headless=HEADLESS,
-            load_timeout=LOAD_TIMEOUT,
-            user_data_dir=USER_DATA_DIR
+            headless=literal_eval(os.environ['HEADLESS']),
+            load_timeout=int(os.environ['LOAD_TIMEOUT']),
+            user_data_dir=os.environ['CHROME_PROFILE']
         )
 
         # Run
         scrapper.sign_in()
-        for keyword in KEYWORDS:
+        for keyword in literal_eval(os.environ['QUERIES']):
             for _ in range(100):
                 try:
                     scrapper.manage_and_run(keyword)
@@ -82,6 +72,8 @@ class Runner(metaclass=Singleton):
                             self._logger.critical(f"Unknown error occurred from scrapper. Exiting!")
                             sys.exit(1)
         self._logger.info("---------------- Crawl process finished successfully! ----------------")
+        scrapper.driver.quit()
+        sys.exit(0)
 
 def run_cpulimit(file_path:str="/home/ubuntu/cpulimit-all.sh",proc_name="chrome",cpu_pct=20):
     if not os.path.exists(file_path):
@@ -99,31 +91,43 @@ def run_cpulimit(file_path:str="/home/ubuntu/cpulimit-all.sh",proc_name="chrome"
     #     return None
     return proc
 
-if __name__ == "__main__":
+def run_with_proc_monitor():
     runner = Runner()
+    logger = getLogger("procmon")
     cpu_limit_process = None
-    # cpu_limit_process = run_cpulimit("/home/ubuntu/cpulimit-all.sh")
-    if cpu_limit_process is None:
-        print("Error in running cpulimit!")
-    for r in range(MAX_SCRAPPER_RESTART_ATTEMPTS):
-        print(f"Attempt number {r+1} in starting the process")
+    high_cpu_threshold = float(os.environ['HIGH_CPU_THRESHOLD'])
+    if high_cpu_threshold > 0:
+        logger.info("Initializing cpulimit")
+        cpu_limit_process = run_cpulimit("/home/ubuntu/cpulimit-all.sh")
+        if cpu_limit_process is None:
+            logger.error("Error in running cpulimit!")
+    for r in range(int(os.environ['MAX_SCRAPPER_RESTART_ATTEMPTS'])):
+        logger.info(f"Attempt number {r+1} in starting the process")
         p = Process(target=runner.run_scrapper)
         p.start()
         sleep(5)
         count = 0
         while p.is_alive():
-            sleep(2)
-            if psutil.cpu_percent(0.1) > HIGH_CPU_THRESHOLD:
+            if high_cpu_threshold > 0  and psutil.cpu_percent(0.5) > high_cpu_threshold:
                 count += 1
-                print(f"Detected high cpu usage. Count={count}")
+                logger.warning(f"Detected high cpu usage. Count={count}")
             else:
                 count = 0
-            if count > MAX_HIGH_CPU_COUNT:
+            if count > int(os.environ['MAX_HIGH_CPU_COUNT']):
                 print("High cpu threshold exceeded. Killing the process")
                 for proc in psutil.process_iter():
                     if proc.name().find("chrome"):
                         proc.kill()
                 p.kill()
+        sleep(5)
+        if p.exitcode and p.exitcode >= 0:
+            logger.info("Process is exited without a problem")
+            break
     if cpu_limit_process is not None:
+        logger.info("Stopping cpulimit")
         cpu_limit_process.kill()
-        
+    logger.info("Done")
+
+
+if __name__ == "__main__":
+    run_with_proc_monitor()
